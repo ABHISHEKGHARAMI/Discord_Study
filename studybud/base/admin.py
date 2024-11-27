d7a3b6cbbd696ee5d6844dffdf02a86be5142c58
abhishek <abhishekgharami1998@gmail.com>
2024-11-27 20:05:07 +0530

    Adding the custom model admin .

--- studybud/base/admin.py
+++ studybud/base/admin.py
@@ -2,4 +2,24 @@ from django.contrib import admin
 from .models import Room, Topic , Message
 
 # Register your models here.
-admin.site.register(Room)
+
+# we will do that with the help of the admin decorator
+
+@admin.register(Topic)
+class TopicAdmin(admin.ModelAdmin):
+    list_display = ['name','slug']
+    prepopulated_fields = {'slug' : ('name',)}
+    
+    
+@admin.register(Room)
+class RoomAdmin(admin.ModelAdmin):
+    list_display = ['name','host','created','updated']
+    list_filter = ['name','created']
+    search_fields = ['name']
+    
+    
+@admin.register(Message)
+class Message(admin.ModelAdmin):
+    list_display = ['user','created','updated']
+    list_filter = ['user','created','updated']
+    search_fields = ['user']
