5876c9a38157deaaba3995b46c823d8139df220f
abhishek <abhishekgharami1998@gmail.com>
2024-11-27 15:42:00 +0530

    Fetching data form the database .

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from  .models import Room
 
 # Create your views here.
 
 
 def home(request):
-    return render(request,'home.html')
+    room = Room.objects.all()
+    # build the context var
+    
+    return render(request,'home.html',{'room':room})
 
 def room(request):
     return render(request,'room.html')
