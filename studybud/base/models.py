7efbcd75563d34fb60e226434a7c07275cb39c90
abhishek <abhishekgharami1998@gmail.com>
2024-11-27 11:02:00 +0530

    Adding the models and adding the database .

--- studybud/base/models.py
+++ studybud/base/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+# first model for the room
+class Room(models.Model):
+    name = models.CharField(max_length=250)
+    desc = models.TextField(null=True,blank=True)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+    
+    
+    def __str__(self):
+        return self.name
+    
