d440d137a903a0200152e4fe9dd3dc1497bc6ebd
abhishek <abhishekgharami1998@gmail.com>
2024-11-28 15:51:16 +0530

    Edit button for the form .

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -37,6 +37,9 @@ def createRoom(request):
 def updateRoom(request,pk):
     room = Room.objects.get(id=pk)
     form = RoomForm(instance=room)
-    
+    if request.method == "POST":
+        form = RoomForm(request.POST,instance=room)
+        form.save()
+        return redirect('base:home')
     context = {'form':form}
     return render(request,'base/room_form.html',context)
