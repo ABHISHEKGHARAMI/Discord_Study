54f6676823937e9ba68f9c1ecdb262b922bb9843
abhishek <abhishekgharami1998@gmail.com>
2024-11-28 15:32:50 +0530

    Added the create  room view for the user .

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from  .models import Room
 from .form import RoomForm
@@ -22,5 +22,10 @@ def room(request,pk):
 # views for creating new form
 def createRoom(request):
     form = RoomForm()
+    if request.method == "POST":
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('base:home')
     context = {'form':form}
     return render(request,'base/room_form.html',context)
