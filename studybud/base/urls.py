315005239cc43df3b2823eff5b2c7cc097767e39
abhishek <abhishekgharami1998@gmail.com>
2024-12-01 13:57:26 +0530

    Adding the user registration for new comers.

--- studybud/base/urls.py
+++ studybud/base/urls.py
@@ -7,6 +7,7 @@ app_name = 'base'
 urlpatterns = [
     path('login/',views.loginPage,name='login'),
     path('logout/',views.logoutPage,name='logout'),
+    path('register/',views.registerPage,name='register'),
     path('',views.home,name='home'),
     path('room/<str:pk>/',views.room,name='room'),
     path('create-room/',views.createRoom,name='create-room'),
